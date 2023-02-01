51f9257fe1396696ede18191aa6a3a0c2bbb66da
Alexandre Meira Lima <64619576+alexandre-ml@users.noreply.github.com>
2023-02-01 19:32:49 -0300

    Adicionar arquivos de projeto.

---
+++ ContosoUniversity/Data/DbInitialize.cs
@@ -0,0 +1,100 @@
+﻿using ContosoUniversity.Models;
+using System.Diagnostics;
+
+namespace ContosoUniversity.Data
+{
+    public static class DbInitialize 
+    {
+        public static void Initializer(SchoolContext context)
+        {
+            // verifica se existem estudantes cadastrados
+            if (context.Students.Any())
+                return; //banco ja foi criado
+
+            AddStudents(context);
+
+            AddCourses(context);
+
+            AddEnrollment(context);
+        }
+
+        public static void AddStudents(SchoolContext c)
+        {
+            //criar estudantes para popular o banco
+            var students = new Student[]
+            {
+                new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+                new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+                new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+                new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+                new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+
+            };
+
+            //adicionar estudantes ao contexto
+            foreach (Student s in students)
+            {
+                c.Students.Add(s);
+            }
+
+            //salvar no banco de dados
+            c.SaveChanges();
+        }
+
+        public static void AddCourses(SchoolContext c)
+        {
+            //criar cursos para popular o banco
+            var courses = new Course[]
+            {
+                new Course{CourseID=1050,Title="Chemistry",Credits=3},
+                new Course{CourseID=4022,Title="Microeconomics",Credits=3},
+                new Course{CourseID=4041,Title="Macroeconomics",Credits=3},
+                new Course{CourseID=1045,Title="Calculus",Credits=4},
+                new Course{CourseID=3141,Title="Trigonometry",Credits=4},
+                new Course{CourseID=2021,Title="Composition",Credits=3},
+                new Course{CourseID=2042,Title="Literature",Credits=4}
+            };
+
+            //adicionar cursos ao contexto
+            foreach (Course course in courses)
+            {
+                c.Courses.Add(course);
+            }
+
+            //salvar no banco de dados
+            c.SaveChanges();
+        }
+
+        public static void AddEnrollment(SchoolContext c)
+        {
+            //criar estudantes para popular o banco
+            var enrollments = new Enrollment[]
+            {
+                new Enrollment{StudentID=1,CourseID=1050,Grid=Grid.A},
+                new Enrollment{StudentID=1,CourseID=4022,Grid=Grid.C},
+                new Enrollment{StudentID=1,CourseID=4041,Grid=Grid.B},
+                new Enrollment{StudentID=2,CourseID=1045,Grid=Grid.B},
+                new Enrollment{StudentID=2,CourseID=3141,Grid=Grid.F},
+                new Enrollment{StudentID=2,CourseID=2021,Grid=Grid.F},
+                new Enrollment{StudentID=3,CourseID=1050},
+                new Enrollment{StudentID=4,CourseID=1050},
+                new Enrollment{StudentID=4,CourseID=4022,Grid=Grid.F},
+                new Enrollment{StudentID=5,CourseID=4041,Grid=Grid.C},
+                new Enrollment{StudentID=6,CourseID=1045},
+                new Enrollment{StudentID=7,CourseID=3141,Grid=Grid.A},
+            };
+
+            //adicionar estudantes ao contexto
+            foreach (Enrollment enrollment in enrollments)
+            {
+                c.Enrollments.Add(enrollment);
+            }
+
+            //salvar no banco de dados
+            c.SaveChanges();
+        }
+    }
+}
---
+++ ContosoUniversity/Program.cs
@@ -0,0 +1,62 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlExpress")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    //inicializar serviços
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        //pegar o context
+        var context = new SchoolContext(
+            services.GetRequiredService<DbContextOptions<SchoolContext>>());
+
+        //iniciar o banco de dados
+        DbInitialize.Initializer(context);
+    }
+    catch(Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger>();
+        logger.LogError(ex, "Um erro ocorreu ao criar a base de dados");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
